ae4928f230595afc1536ad6a8ea5a2e6acd4e325
nicogauthierpin <nicolas@gauthier-pin.com>
2020-07-20 00:42:04 -0700

    Content updates and mobile styles

--- script.js
+++ script.js
@@ -26,7 +26,7 @@ Vue.component('about-page', {
       <p>I am a self-taught front-end web developer, with a background on graphic design.</p>\
       <p>My schooling background is in Engineering. I was one of those kids who liked to disassemble things to see how they work. I still like to do it, but I quickly realized I didn’t want to make a career out of it. Instead, the web allowed me to merge my passion for building things and creating graphics.</p>\
       <p>I was born and raised in Argentina, and lived in London, Montréal, San Francisco, and now LA. I’ve also spent significant time in New York and Toronto. I am 100% bilingual in english and spanish, and I can speak beginner italian and just a tiny little bit of french.</p>\
-      <p>Other things I like: <a href="#">Things</a> <a href="#">with</a> <a href="#">wheels</a>, <a href="https://www.goodreads.com/user/show/5498039-nico">books</a>, coffee, good beer.</p>\
+      <p>Other things I like: <span class="funfact"><span class="tooltip"><img src="img/bikes.jpg"></span>Things</span> <span class="funfact"><span class="tooltip"><img src="img/bmw.jpg"></span>with</span> <span class="funfact"><span class="tooltip"><img src="img/saab.jpg"></span>wheels</span>, <span class="funfact"><span class="tooltip"><img src="img/books.jpg"></span>books</span>, <span class="funfact"><span class="tooltip"><img src="img/coffee.jpg"></span>coffee</span>, <span class="funfact"><span class="tooltip"><img src="img/beer.jpg"></span>good beer<span>.</p>\
       <h2>What am I up to</h2>\
       <p>I am currently the Interactive Manager at Gunn | Jerkens, a marketing agency in Long Beach, CA, where we build websites and interactive experiences for real estate developers. Prior to that I freelanced for over 10 years, working with clients ranging from individuals to large corporations, in at least four different continents.</p>\
       <p>On the side, I am active in the local Wordpress and developer community in Long Beach. I am one of the organizers of the Long Beach chapter of the <a href="https://www.meetup.com/WPLosAngeles/">Wordpress Los Angeles</a> meetup, I was part of the organizing team of <a href="https://2019.longbeach.wordcamp.org/">WordCamp Long Beach 2019</a>, and I am a regular attendant of the <a href="http://codeandcoffeelb.org/">Code&&Coffee meetup</a>. I\'m also involved in the <a href="http://longbeachcommunitytheater.com/">Long Beach Community Theater</a>, lending a hand with everything web and whatever else is needed.</p>\
@@ -39,8 +39,9 @@ Vue.component('social', {
     <aside id="social" class="inset">\
       <a href="https://www.facebook.com/nicolaspin"><img src="img/facebook-with-circle.svg" alt="Facebook"></a>\
       <a href="https://www.instagram.com/nicogauthierpin/"><img src="img/instagram-with-circle.svg" alt="Instagram"></a>\
+      <a href="https://www.goodreads.com/user/show/5498039-nico"><img src="img/goodreads-with-circle.svg" alt="Goodreads"></a>\
       <a href="https://github.com/nicogauthierpin"><img src="img/github-with-circle.svg" alt="Github"></a>\
-      <a href="www.linkedin.com/in/nicogauthierpin"><img src="img/linkedin-with-circle.svg" alt="LInkedin"></a>\
+      <a href="https://www.linkedin.com/in/nicogauthierpin"><img src="img/linkedin-with-circle.svg" alt="Linkedin"></a>\
     </aside>'
 });
 
